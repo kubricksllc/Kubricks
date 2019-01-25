909ef14a34bd0f147ce10aaca9ee345054829938
Taihyun Lee <th9061@hotmail.com>
2019-01-25 17:03:07 -0500

    added infowindow tree

--- src/client/components/info_window/InfoPane.jsx
+++ src/client/components/info_window/InfoPane.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from "react-redux";
+import ReactJson from 'react-json-view';
 
 const InfoTitle = styled.h1`
   text-decoration: underline;
@@ -11,11 +12,32 @@ class InfoPane extends Component {
     super();
   }
 
-  render() {    
+  renderPropsObject() {
+
+  }
+
+  render() { 
+    let rjvConfig = {
+      src: {},
+      name: false,
+      theme: "monokai",
+      enableClipboard: false,
+      displayObjectSize: false,
+      displayDataTypes: false
+    }; 
+
+    if(this.props.currentNode) {
+      rjvConfig.src = this.props.currentNode;
+    } else if(this.props.currentService) {
+      rjvConfig.src = this.props.currentService;
+    } else if(this.props.currentPod) {
+      rjvConfig.src = this.props.currentPod;
+    }
+
     return (
       <div>
         <InfoTitle>Info Window</InfoTitle>
-        
+        <ReactJson {...rjvConfig}/>
       </div>
     )
   }
