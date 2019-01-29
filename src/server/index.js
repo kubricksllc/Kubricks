748c69ac34061dac5bb5c68e3f6ce5faddc4481c
Taihyun Lee <th9061@hotmail.com>
2019-01-29 10:52:23 -0500

    merged with electron

--- src/server/index.js
+++ src/server/index.js
@@ -21,6 +21,8 @@ app.get('/api/renderall', nodeQuery.getServicesAndPods, (req, res) =>
   res.end()
 );
 
+app.get('/api/pv/*', pvQuery.getPV, (req, res) => res.end());
+
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../main/index.html"));
 });
